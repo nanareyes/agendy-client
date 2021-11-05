8d31cfeb87fa42fa6e6b76a59dbb48f10bbd7df9
subcorporeal <subcorporeal@gmail.com>
2021-11-04 23:17:25 -0500

    arreglos de algunos estilos

--- src/App.vue
+++ src/App.vue
@@ -7,99 +7,135 @@
       <router-link to="/portafolioServicios">Servicios</router-link>
     </div>  -->
 
-    <router-view/>
+    <router-view />
     <footer>
-    <div >
-      <div class="row">
-        <div class="col-6 col-xs-12 col-sm-12 col-md-6">
-          <div class="row mx-2">
-            <h1>Acerca de Nosotros</h1>
-            <div class="texto">
-                <p> El Centro de estética ANGENDY NAILS, ofrece un espacio de armonía y confort, atendido por personal altamente calificado, ofreciendo el embellecimiento
-                    de uñas con diferentes tipos de diseños, colores, texturas y técnicas.
-                </p><br>
+      <div>
+        <div class="row">
+          <div class="col-6 col-xs-12 col-sm-12 col-md-6">
+            <div class="row mx-2">
+              <h1>Acerca de Nosotros</h1>
+              <div class="texto">
+                <p>
+                  El Centro de estética ANGENDY NAILS, ofrece un espacio de
+                  armonía y confort, atendido por personal altamente calificado,
+                  ofreciendo el embellecimiento de uñas con diferentes tipos de
+                  diseños, colores, texturas y técnicas.
+                </p>
+                <br />
+              </div>
+            </div>
+            <div class="row mx-2">
+              <div class="integrantes">
+                <h5>Integrantes Grupo 4</h5>
+                <br />
+                <li>Yenny Natacha Libreros Montes</li>
+                <li>Diana Carolina Reyes Mantillla</li>
+                <li>Sonia Milena Mosquera</li>
+                <li>Monica Colorado Romero</li>
+                <li>Christian Alexander Velasco</li>
+              </div>
             </div>
           </div>
-          <div class="row mx-2">
-            <div class="integrantes">
-              <h5>Integrantes Grupo 4</h5><br>
-              <li>Yenny Natacha Libreros Montes</li>
-              <li>Diana Carolina Reyes Mantillla</li>
-              <li>Sonia Milena Mosquera</li>
-              <li>Monica Colorado Romero</li>
-              <li>Christian Alexander Velasco</li>
+          <div class="col-3 col-xs-12 col-sm-12 col-md-3">
+            <h5>Redes Sociales</h5>
+            <div class="row">
+              <div class="col-1">
+                <i class="fab fa-facebook fa-lg"></i>
+              </div>
+              <div class="col-11 text-start">
+                <a href="https://facebook.com/" target="_blank"
+                  ><label>Siguenos en Facebook</label></a
+                >
+              </div>
+            </div>
+            <div class="row mt-2">
+              <div class="col-1">
+                <i class="fab fa-twitter fa-lg"></i>
+              </div>
+              <div class="col-11 text-start">
+                <a href="https://twitter.com/" target="_blank"
+                  ><label>Siguenos en Twitter</label></a
+                >
+              </div>
+            </div>
+            <div class="row mt-2">
+              <div class="col-1">
+                <i class="fab fa-instagram fa-lg"></i>
+              </div>
+              <div class="col-11 text-start">
+                <a href="https://instagram.com/" target="_blank"
+                  ><label>Siguenos en Instagram</label></a
+                >
+              </div>
+            </div>
+            <div class="row mt-2">
+              <div class="col-1">
+                <i class="fab fa-pinterest fa-lg"></i>
+              </div>
+              <div class="col-11 text-start">
+                <a href="https://pinterest.com/" target="_blank"
+                  ><label>Siguenos en Pinterest</label></a
+                >
+              </div>
+            </div>
+          </div>
+
+          <div class="col-3 col-xs-12 col-sm-12 col-md-3">
+            <h5>Informacion Contacto</h5>
+            <!-- <div class="row">
+              <div class="col-12 text-start">
+                <i class="fa-solid fa-house fa-lg mx-3"></i><label></label>
+              </div>
+            </div> -->
+
+            <div class="row">
+              <div class="col-12">
+                <ul class="phone">
+                  <li>
+                    <i class="fa-solid fa-phone fa-lg mx-3"></i>320-692-2383
+                  </li>
+                  <li>
+                    <i class="fa-solid fa-phone fa-lg mx-3"></i>320-748-3074
+                  </li>
+                  <li>
+                    <i class="fa-solid fa-phone fa-lg mx-3"></i>318-697-7658
+                  </li>
+                  <li>
+                    <i class="fa-solid fa-phone fa-lg mx-3"></i>319-572-3583
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-12 text-start">
+                <i class="fa-solid fa-envelope fa-lg mx-3"></i
+                ><label>email@email.com</label>
+              </div>
             </div>
           </div>
         </div>
-        <div class="col-3 col-xs-12 col-sm-12 col-md-3">
-          <h5>Redes Sociales</h5>
-          <div class="row ">
-            <div class="col-1">
-              <i class="fab fa-facebook fa-lg"></i>
-            </div>
-            <div class="col-11 text-start"> <a href="https://facebook.com/" target="_blank"><label>Siguenos en Facebook</label></a></div>
-          </div>
-          <div class="row mt-2">
-            <div class="col-1">
-              <i class="fab fa-twitter fa-lg"></i>
-            </div>
-            <div class="col-11 text-start"> <a href="https://twitter.com/" target="_blank"><label>Siguenos en Twitter</label></a></div>
-          </div>
-          <div class="row mt-2">
-            <div class="col-1">
-              <i class="fab fa-instagram fa-lg"></i>
-            </div>
-            <div class="col-11 text-start"> <a href="https://instagram.com/" target="_blank"><label>Siguenos en Instagram</label></a></div>
-          </div>
-          <div class="row mt-2">
-            <div class="col-1">
-              <i class="fab fa-pinterest fa-lg"></i>
-            </div>
-            <div class="col-11 text-start"> <a href="https://pinterest.com/" target="_blank"><label>Siguenos en Pinterest</label></a></div>
-          </div>
-        </div>
 
-
-        <div class="col-3 col-xs-12 col-sm-12 col-md-3">
-          <h5> Informacion Contacto</h5>
-          <div class="row">
-            <div class="col-12 text-start"> <i class="fa-solid fa-house fa-lg mx-3"></i><label>Aca se coloca la direccion de la empresa</label> </div>
-          </div>
-
-          <div class="row">
-            
-            <div class="col-12">
-              <ul class="phone">
-                <li><i class="fa-solid fa-phone fa-lg mx-3" ></i>+57 320 692 2383</li>
-                <li><i class="fa-solid fa-phone fa-lg mx-3"></i>+57 320 748 3074</li>
-                <li><i class="fa-solid fa-phone fa-lg mx-3"></i>+57 300 477 7527</li>
-                <li><i class="fa-solid fa-phone fa-lg mx-3"></i>+57 319 245 5093</li>
-              </ul>
+        <div class="container-footer2 foot2 row">
+          <div class="footer2">
+            <div class="copyright">
+              © Todos los derechos reservados |
+              <a
+                href="https://github.com/nanareyes/Proyecto-Ciclo-3/blob/development/index.html"
+                target="_blanck"
+                >Acceso al Git</a
+              >
             </div>
-          </div>
-
-          <div class="row">
-            <div class="col-12 text-start"><i class="fa-solid fa-envelope fa-lg mx-3"></i><label>email@email.com</label></div>
+            <div class="information">
+              <a href=""> Informacion Compañia |</a>
+              <a href="">Privacion y Politica |</a>
+              <a href="">Terminos y condiciones</a>
+            </div>
           </div>
         </div>
       </div>
-
-      <div class="container-footer2 foot2 row ">
-        <div class="footer2">
-          <div class="copyright">© Todos los derechos reservados |
-            <a href="https://github.com/nanareyes/Proyecto-Ciclo-3/blob/development/index.html" target="_blanck">Acceso al Git</a>
-          </div>
-          <div class="information">
-            <a href=""> Informacion Compañia |</a>
-            <a href="">Privacion y Politica |</a>
-            <a href="">Terminos y condiciones</a>
-          </div>
-        </div>
-      </div>
-    </div>
     </footer>
   </div>
-  
 </template>
 
 <style>
@@ -122,6 +158,10 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.card-title {
+  font-weight: bold;
 }
 </style>
 
